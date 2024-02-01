1bb42decaea61fb0bd703c3b0b4d13b1c726f73b
Mickael Riss <mendos.prod@gmail.com>
2024-02-01 00:08:46 -0500

    Almost finish the Banner need to fix responsive

---
+++ src/components/Home/TechStackItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-html="stack.icon" :alt="stack.name"></div>
+</template>
+
+<script>
+export default {
+  name: 'TechStackItem',
+  props: {
+    stack: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
