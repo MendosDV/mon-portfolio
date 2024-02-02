572389b37e36ca040d033d422408a1f4def65d3f
Mickael Riss <mendos.prod@gmail.com>
2024-02-02 00:29:22 -0500

    change

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <NavbarApp />
-  <BannerApp />
-  <AboutApp ref="aboutSection" id="about-section" />
-  <ProjectApp ref="projectsSection" id="projects-section" />
-  <ContactApp ref="contactSection" id="contact-section" />
+  <HomeView />
   <FooterApp />
 </template>
 
 <script>
 import NavbarApp from './components/NavbarApp.vue'
-import BannerApp from './components/Home/Banner/BannerApp.vue'
-import AboutApp from './components/Home/AboutApp.vue'
-import ProjectApp from './components/Home/ProjectApp.vue'
-import ContactApp from './components/Home/ContactApp.vue'
 import FooterApp from './components/FooterApp.vue'
-
+import HomeView from './views/Home.vue'
 
 export default {
   name: 'App',
-  // mounted() {
-  //   this.$refs.aboutSection.$el.scrollIntoView();
-  //   window.scrollBy(30, -80);
-  // },
-  components: { NavbarApp, BannerApp, AboutApp, ProjectApp, ContactApp, FooterApp },
+  components: { NavbarApp, FooterApp, HomeView },
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+  <!-- bon -->
   <div>
     <section id="banner">
       <BannerApp/>
